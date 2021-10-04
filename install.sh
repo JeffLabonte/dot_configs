b490fb8cf7fabaa1f89c45fd1423b8b3684e7469
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-03 23:06:19 -0400

    Update install.sh file

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 NVIM_PATH="${HOME}/.config/nvim"
+TREE_VIM_PATH="${HOME}/tree.nvim"
 
-mkdir ${HOME}/tree.nvim && cd ${HOME}/tree.nvim && \ 
-sh -c "$(wget -O- https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)" && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)"
+mkdir ${TREE_VIM_PATH} && \ 
+    cd ${TREE_VIM_PATH} && sh $(wget -O- https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)  && \
+    cd ${TREE_VIM_PATH} && sh $(curl -fsSL https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)
 
 if [ ! -d "${NVIM_PATH}" ]; then
     mkdir "${NVIM_PATH}"
@@ -12,6 +13,7 @@ fi
 
 cp dot_config/nvim/* "${NVIM_PATH}"
 
-# Install Packages
+pipx install 'python-lsp-server[all]'
+npm i -g pyright
 
 nvim +PlugInstall +q

74e383546e3cbb1cf7ce77bcce88e6ea16138ed0
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-03 23:15:57 -0400

    Update script to make it more efficient

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-fn backup () {
+function backup () {
     cp -rf $1 $1.back
 }
 
 NVIM_PATH="${HOME}/.config/nvim"
 TREE_VIM_PATH="${HOME}/tree.nvim"
 
-backup $NVIM_PATH
-backup $TREE_VIM_PATH
+backup "${NVIM_PATH}"
+backup "${TREE_VIM_PATH}"
 
-mkdir ${TREE_VIM_PATH} && \ 
+if [ ! -d "${TREE_VIM_PATH}" ]; then
     cd ${TREE_VIM_PATH} && sh $(wget -O- https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)  && \
     cd ${TREE_VIM_PATH} && sh $(curl -fsSL https://raw.githubusercontent.com/zgpio/tree.nvim/master/install.sh)
+fi
 
 if [ ! -d "${NVIM_PATH}" ]; then
     mkdir "${NVIM_PATH}"
